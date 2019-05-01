be4216f4eb8ae4dd2640b7aabe9c8dddeccb30d9
Jeff Dam <jeff.dam@gmail.com>
2019-05-01 14:06:50 -0700

    Push to Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create({username: "demo_user", email: "demo@demo.com", password:"password"})
