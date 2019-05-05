83db533b24ece341f29b8f2c8705fa2c66cc1981
Jeff Dam <jeff.dam@gmail.com>
2019-05-05 11:16:04 -0700

    Create routes controller

---
+++ app/controllers/api/routes_controller.rb
@@ -0,0 +1,58 @@
+class Api::RoutesController < ApplicationController
+
+  def index
+    @routes = Route.where(parent_id: nil)
+  end
+
+  def show
+    @route = Route.find(params[:id])
+  end
+
+  def create
+    @route = Route.new(route_params)
+
+    if @route.save
+      render :show
+    else
+      render json: @route.errors.full_messages, status: 401
+    end
+  end
+
+  def update    
+    @route = Route.find(params[:id])
+
+    if @route.update(route_params)
+      render :show
+    else
+      render json: @route.errors.full_messages, status: 401
+    end
+  end
+
+  def destroy
+    @route = Route.find(params[:id])
+  end
+
+  private
+  
+  def route_params
+    params.require(:route).permit(
+      :name, 
+      :route_type, 
+      :author_id, 
+      :area_id, 
+      :grade, 
+      :safety, 
+      :first_ascensionist, 
+      :first_ascent_date, 
+      :length, 
+      :pitches, 
+      :protection, 
+      :description, 
+      :getting_there
+    )
+  end
+end
+
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :show]
     resource :session, only: [:create, :destroy]
-    resources :areas, only: [:index, :create, :show, :update, :destroy]
+    resources :areas, only: [:index, :create, :show, :update]
+    resources :routes, only: [:index, :create, :show, :update]
   end
   
   root to: 'static_pages#root' 
