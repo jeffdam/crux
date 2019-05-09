2377d00d708ac38d198f83edd1984d1a545b36c5
Jeff Dam <jeff.dam@gmail.com>
2019-05-09 12:21:13 -0700

    Update route finder

--- frontend/components/routes/route_finder/route_finder.jsx
+++ frontend/components/routes/route_finder/route_finder.jsx
@@ -15,19 +15,83 @@ class RouteFinder extends React.Component {
     if (routes === {}) return null;
 
     const searchParams = queryString.parse(this.props.location.search);
+
+
+    const ropeGrades = [
+      "5.0", "5.1", "5.2", "5.3", "5.4", "5.5", "5.6",
+      "5.7-", "5.7", "5.7+", "5.8-", "5.8", "5.8+", "5.9-", "5.9", "5.9+",
+      "5.10-", "5.10a", "5.10a/b", "5.10b", "5.10b/c", "5.10c", "5.10c/d", "5.10d", "5.10+",
+      "5.11-", "5.11a", "5.11a/b", "5.11b", "5.11b/c", "5.11c", "5.11c/d", "5.11d", "5.11+",
+      "5.12-", "5.12a", "5.12a/b", "5.12b", "5.12b/c", "5.12c", "5.12c/d", "5.12d", "5.12+",
+      "5.13-", "5.13a", "5.13a/b", "5.13b", "5.13b/c", "5.13c", "5.13c/d", "5.13d", "5.13+",
+      "5.14-", "5.14a", "5.14a/b", "5.14b", "5.14b/c", "5.14c", "5.14c/d", "5.14d", "5.14+",
+      "5.15-", "5.15a", "5.15a/b", "5.15b", "5.15b/c", "5.15c", "5.15c/d", "5.15d", "5.15+",
+      "5.16"
+    ];
+
+    const gradeConvert = grade => {
+      if (grade.includes(" ")) {
+        return grade.split(" ").join("") + "+";
+      } else {
+        return grade;
+      }
+    };
     
+    const minRopeGradeIdx = ropeGrades.indexOf(gradeConvert(searchParams.r_grade_min));
+    const maxRopeGradeIdx = ropeGrades.indexOf(gradeConvert(searchParams.r_grade_max)) + 1;
+
+    const boulderGrades = [
+      "VB", "V0-", "V0", "V0+", "V1-", "V1", "V1+", "V2-", "V2", "V2+", "V3-", "V3", "V3+", "V4-", "V4",
+      "V4+", "V5-", "V5", "V5+", "V6-", "V6", "V6+", "V7-", "V7", "V7+", "V8-", "V8", "V8+", "V9-", "V9",
+      "V9+", "V10-", "V10", "V10+", "V11-", "V11", "V11+", "V12-", "V12", "V12+", "V13-", "V13", "V13+",
+      "V14-", "V14", "V14+", "V15-", "V15", "V15+", "V16-", "V16", "V16+", "V17-", "V17", "V17+",
+    ];
+
+    const minBoulderGradeIdx = boulderGrades.indexOf(searchParams.b_grade_min);
+    const maxBoulderGradeIdx = boulderGrades.indexOf(searchParams.b_grade_max) + 1;
+
+    const typeSearch = route => {
+      return [searchParams.is_t, searchParams.is_s, searchParams.is_b].includes(route.routeType);
+    };
+
+    const topropeSearch = route => {
+      if (searchParams.is_tr === 'true') {
+        return route.toprope === true;
+      } else {
+        return route.toprope === true || route.toprope === false;
+      }
+    };
+
+    const pitchSearch = route => {
+      return (route.pitches >= searchParams.pitches);
+    };
+
+    console.log(searchParams);
+    console.log(ropeGrades.slice(minRopeGradeIdx, maxRopeGradeIdx));
+    
+    const ropeGradeSearch = route => {
+      const ropeGrade = route.grade.split(" ")[0];
+      console.log(ropeGrade);
+      return ropeGrades.slice(minRopeGradeIdx, maxRopeGradeIdx).includes(ropeGrade);
+    };
+
     const filteredResults = routes
-      .filter(route => [searchParams.is_t, searchParams.is_s, searchParams.is_b].includes(route.routeType))
-      .filter(route => route.toprope === searchParams.is_tr);
+      .filter(route => 
+        typeSearch(route) && 
+        topropeSearch(route) &&
+        ropeGradeSearch(route)
+        );
+
 
     const routesList = filteredResults.map(route => {
       const area = areas[route.areaId];
+      const tr = route.toprope ? ", TR" : "";
       return (
         <ul key={route.id} className="route-finder-info flex-row">
           <li><Link to={`/routes/${route.id}`}>{route.name}</Link></li>
           <li><Link to={`/areas/${area.id}`}>{area.name}</Link></li>
           <li>{route.grade}{` ${route.safety}`}</li>
-          <li>{route.routeType}</li>
+          <li>{route.routeType}{tr}</li>
           <li>{route.pitches}</li>
         </ul>
       )
--- frontend/components/routes/route_finder/route_finder_form.jsx
+++ frontend/components/routes/route_finder/route_finder_form.jsx
@@ -14,7 +14,7 @@ class RouteFinderForm extends React.Component {
       r_grade_max: "5.16",
       b_grade_min: "VB",
       b_grade_max: "V17+",
-      pitches: "max",
+      pitches: 1,
       sort_by: "name"
     };
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -22,8 +22,8 @@ class RouteFinderForm extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    // this.props.history.push(`/route-finder?location=${location}&is_b=${is_b}&is_t=${is_t}&is_s=${is_s}&is_tr=${is_tr}&r_grade_min=${r_grade_min}&r_grade_max=${r_grade_max}&b_grade_min=${b_grade_min}&b_grade_max=${b_grade_max}&pitches=${pitches}&sort_by=${sort_by}`);
-    this.props.history.push(`/route-finder?is_b=${this.state.is_b}&is_t=${this.state.is_t}&is_s=${this.state.is_s}&is_tr=${this.state.is_tr}`);
+    // this.props.history.push(`/route-finder?location=${location}&is_b=${is_b}&is_t=${is_t}&is_s=${is_s}&is_tr=${is_tr}&b_grade_min=${b_grade_min}&b_grade_max=${b_grade_max}&pitches=${pitches}&sort_by=${sort_by}`);
+    this.props.history.push(`/route-finder?is_b=${this.state.is_b}&is_t=${this.state.is_t}&is_s=${this.state.is_s}&is_tr=${this.state.is_tr}&r_grade_min=${this.state.r_grade_min}&r_grade_max=${this.state.r_grade_max}`);
   }
 
   update(field) {
@@ -52,26 +52,83 @@ class RouteFinderForm extends React.Component {
         } else {
           this.setState({ ["is_tr"]: false });
         }
-      } 
+      } else {
+        this.setState({ [field]: e.target.value });
+      }
     };
   }
 
+
+
   render() {
+
+    const ropeGrades = [
+      "5.0", "5.1", "5.2", "5.3", "5.4", "5.5", "5.6",
+      "5.7-", "5.7", "5.7+", "5.8-", "5.8", "5.8+", "5.9-", "5.9", "5.9+",
+      "5.10-", "5.10a", "5.10a/b", "5.10b", "5.10b/c", "5.10c", "5.10c/d", "5.10d", "5.10+",
+      "5.11-", "5.11a", "5.11a/b", "5.11b", "5.11b/c", "5.11c", "5.11c/d", "5.11d", "5.11+",
+      "5.12-", "5.12a", "5.12a/b", "5.12b", "5.12b/c", "5.12c", "5.12c/d", "5.12d", "5.12+",
+      "5.13-", "5.13a", "5.13a/b", "5.13b", "5.13b/c", "5.13c", "5.13c/d", "5.13d", "5.13+",
+      "5.14-", "5.14a", "5.14a/b", "5.14b", "5.14b/c", "5.14c", "5.14c/d", "5.14d", "5.14+",
+      "5.15-", "5.15a", "5.15a/b", "5.15b", "5.15b/c", "5.15c", "5.15c/d", "5.15d", "5.15+",
+      "5.16"
+    ]
+    const ropeGradeMinInput = ropeGrades.map(ropeGrade => {
+      return (
+        <option
+          key={ropeGrade}
+          value={ropeGrade}
+        >{ropeGrade}</option>
+      )
+    })
+
+    const ropeGradeMaxInput = ropeGrades.slice(ropeGrades.indexOf(this.state.r_grade_min)).map(ropeGrade => {
+      return (
+        <option
+          key={ropeGrade}
+          value={ropeGrade}
+        >{ropeGrade}</option>
+      )
+    })
+
+
     return (
       <form onSubmit={this.handleSubmit}>
-         <label>Trad
-          <input id="t" onChange={this.update('t')} type="checkbox" value='true'/>
-         </label>
-         <label>Sport
-          <input id="s" onChange={this.update('s')} type="checkbox" value='true'/>
-         </label>
-         <label>Boulder
-          <input id="b" onChange={this.update('b')} type="checkbox" value='true'/>
-         </label>
-         <label>Top Rope
-          <input id="tr" onChange={this.update('tr')} type="checkbox" value='true'/>
-         </label>
-         <input type="submit" value="Find Routes"/>
+        <label>Trad
+        <input id="t" onChange={this.update('t')} type="checkbox" value='true'/>
+        </label>
+        <label>Sport
+        <input id="s" onChange={this.update('s')} type="checkbox" value='true'/>
+        </label>
+        <label>Boulder
+        <input id="b" onChange={this.update('b')} type="checkbox" value='true'/>
+        </label>
+        <label>Top Rope
+        <input id="tr" onChange={this.update('tr')} type="checkbox" value='true'/>
+        </label>
+        <label>Rope Grade
+          <label>Min:
+            <select onChange={this.update("r_grade_min")}>
+              <option value="">--</option>
+              {ropeGradeMinInput}
+            </select>
+          </label>
+          <label>Max:
+            <select onChange={this.update("r_grade_max")}>
+              <option value="">--</option>
+              {ropeGradeMaxInput}
+            </select>
+
+          </label>
+        </label>
+
+        {/* <label>
+          <select onChange={this.update("pitches")}>
+            <option value="1">--</option>
+            <
+          </select>
+        </label> */}
+        <input type="submit" value="Find Routes"/>
       </form>
     )
   }
