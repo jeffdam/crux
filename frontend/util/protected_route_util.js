ea9e2f94bae3691bb9ab5b31c13472726c620948
Jeff Dam <jeff.dam@gmail.com>
2019-06-20 22:00:17 -0700

    Refactor code

--- frontend/util/protected_route_util.js
+++ frontend/util/protected_route_util.js
@@ -1,7 +1,6 @@
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { withRouter, Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 const Auth = ({ component: Component, path, loggedIn, exact }) => (
   <Route path={path} exact={exact} render={(props) => (
