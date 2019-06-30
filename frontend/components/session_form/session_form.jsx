8712bbf4e9a07027cb91f211fd4073e25ee82119
Jeff Dam <jeff.dam@gmail.com>
2019-06-30 01:45:36 -0700

    Remove code

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { closeModal } from '../../actions/modal_actions';
 
 class SessionForm extends React.Component {
   constructor(props) {
