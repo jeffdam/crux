c03d01d674c0e253fa3ad2726e354a8cec8b35dc
Jeff Dam <jeff.dam@gmail.com>
2019-08-01 23:27:36 -0700

    Update footer links

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -4,8 +4,10 @@ const Footer = () => (
   <footer className="footer-main">
     <section className="footer-container main-width">
       <div className="flex-row">
-        <a href="https://www.linkedin.com/in/jeffdam" target="_blank"><img src={window.images.linkedin_logo} alt="LinkedIn Logo" ></img></a>
-        <a href="https://github.com/jeffdam" target="_blank"><img src={window.images.github_logo} alt="GitHub Logo" ></img></a>
+        <a href="https://www.linkedin.com/in/jeffdam" target="_blank"><img src={window.images.linkedin_logo} alt="LinkedIn Logo" title="LinkedIn"></img></a>
+        <a href="https://github.com/jeffdam" target="_blank"><img src={window.images.github_logo} alt="GitHub Logo" title="GitHub"></img></a>
+        <a href="https://angel.co/jeff-dam/" target="_blank"><img src={window.images.angellist_logo} alt="angellist_logo" title="AngelList"></img></a>
+        <a href="https://jeffdam.com/" target="_blank"><img src={window.images.portfolio_logo} alt="portfolio" title="Personal Website"></img></a>
       </div>
     </section>
   </footer>
