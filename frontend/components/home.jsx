549218ec85c86f06962c0b361548921aa3e03181
Jeff Dam <jeff.dam@gmail.com>
2019-08-22 22:38:45 -0700

    Add route finder to home page header

--- frontend/components/home.jsx
+++ frontend/components/home.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import AreasIndexContainer from './areas/areas_index_container';
 import AreasIndexHeader from './areas/areas_index_header';
+import RouteFinderForm from './routes/route_finder/route_finder_form';
+
 
 const Home = () => (
   <section>
     <AreasIndexHeader />
+    <div className="home-header">
+      <div>
+        <RouteFinderForm />
+      </div>
+    </div>
     <AreasIndexContainer />
   </section>
 );
