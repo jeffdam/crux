cf9921754e35369dce3348347c1eb3e36cb0a4fe
Jeff Dam <jeff.dam@gmail.com>
2019-09-15 22:42:51 -0700

    Refactor code

--- frontend/components/routes/route_header/route_new_list.jsx
+++ frontend/components/routes/route_header/route_new_list.jsx
@@ -1,59 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-class RouteNewList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const RouteNewList = (props) => {
+  const { routes } = props;
+  const routeLinks = Object.values(routes).map((route, idx) => {
+    return (
+      <li key={idx}>
+        <ul className="new-routes-list-info">
+          <li title={route.name}>
+            <Link to={`/routes/${route.id}`}>{route.name}</Link>
+          </li>
+          <li title={route.area.name}>
+            <Link to={`/areas/${route.area.id}`}>{route.area.name}</Link>
+          </li>
+          <li>
+            {route.grade}
+          </li>
+          <li>
+            {route.routeType}
+          </li>
+        </ul>
+      </li>
+    )
+  })
 
-  componentDidMount() {
-  }
+  return (
 
-  componentDidUpdate(prevProps) {
-  }
-
-  render() {
-    const { routes } = this.props;
-    const routeLinks = Object.values(routes).map((route, idx) => {
-      return (
-        <li key={idx}>
+    <section className="new-routes-list-main">
+      <h3>Recently Added Routes</h3>
+      <ul className="new-routes-list">
+        <li>
           <ul className="new-routes-list-info">
-            <li title={route.name}>
-              <Link to={`/routes/${route.id}`}>{route.name}</Link>
-            </li>
-            <li title={route.area.name}>
-              <Link to={`/areas/${route.area.id}`}>{route.area.name}</Link>
-            </li>
-            <li>
-              {route.grade}
-            </li>
-            <li>
-              {route.routeType}
-            </li>
+            <li><h4>Name</h4></li>
+            <li><h4>Area</h4></li>
+            <li><h4>Grade</h4></li>
+            <li><h4>Type</h4></li>
           </ul>
         </li>
-      )
-    })
-
-    return (
-
-      <section className="new-routes-list-main">
-        <h3>Recently Added Routes</h3>
-        <ul className="new-routes-list">
-          <li>
-            <ul className="new-routes-list-info">
-              <li><h4>Name</h4></li>
-              <li><h4>Area</h4></li>
-              <li><h4>Grade</h4></li>
-              <li><h4>Type</h4></li>
-            </ul>
-          </li>
-          {routeLinks}
-        </ul>
-      </section>
-    )
-  }
+        {routeLinks}
+      </ul>
+    </section>
+  )
 }
-
 
 export default RouteNewList;
